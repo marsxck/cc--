3ab797a179967da8d9bfb4352dc324268a005755
marsxck <marsxck@users.noreply.github.com>
2018-07-24 20:15:50 +0800

    一道题

--- mergersort/mergersort/merger.cpp
+++ mergersort/mergersort/merger.cpp
@@ -46,9 +46,25 @@ void mergersort(int arr[],int low,int high)
 	mergersort(arr, mid + 1, high);
 	merger(arr,low,high);
 }
+int fun(int a[], int b, int e)
+{
+	if (e - b <= 1)
+		return abs(a[b] -a[e]) >= 3 ? a[b] : a[e];
+	int l = 0, r = 0;
+	l = fun(a, b, b + (e - b) / 2);
+	if (l % 2 == 0)
+		r = fun(a, b + (e - b) / 2 + 1, e);
+	else
+		return 1;
+	if (1 | r)
+		return 1 | r;
+	else
+		return r;
+}
 int main()
 {
-	int a[] = { 4,1,2,3 };
-	mergersort(a,0,3);
+	int a[] = { 10,5,8,4,5,20,2,3 };
+	std::cout << fun(a, 0, 8);
+	//mergersort(a,0,3);
 	return 0;
 }
